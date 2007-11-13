2ff3b58dd9f8644fe057b3dc919ab453ff9c55b8
damien.baty <damien.baty@809b8a20-6c76-11dc-b6fd-a992d7edf03f>
2007-11-13 23:34:18 +0000

    Moved doctest to '/tests/test_shared.py'.

--- src/ximenez/shared/ssh.py
+++ src/ximenez/shared/ssh.py
@@ -60,64 +60,7 @@ class SSHRemoteHost(object):
 
 def escapeShellCommand(command,
                        special_chars=""";&|!><~*{}[]?()$\\`"""):
-    """Escape special shell characters from ``command``.
-
-    >>> f = escapeShellCommand
-    >>> print f('ls')
-    ls
-    >>> print f('cd /var ; ls')
-    cd /var \; ls
-    >>> print f('cd && ls')
-    cd \&\& ls
-    >>> print f('cd || ls')
-    cd \|\| ls
-    >>> print f('ls &')
-    ls \&
-    >>> print f('ls | grep foo')
-    ls \| grep foo
-    >>> print f('echo Foo!')
-    echo Foo\!
-    >>> print f('echo !!')
-    echo \!\!
-    >>> print f('ls > foo')
-    ls \\> foo
-    >>> print f('ls < foo')
-    ls \< foo
-    >>> print f('ls ~')
-    ls \~
-    >>> print f('ls *')
-    ls \*
-    >>> print f('ls {foo,bar}.txt')
-    ls \{foo,bar\}.txt
-    >>> print f('ls [fb]oo')
-    ls \[fb\]oo
-    >>> print f('ls ?oo')
-    ls \?oo
-    >>> print f('(ls)')
-    \(ls\)
-    >>> print f('echo US$380')
-    echo US\$380
-    >>> print f('echo 1\\2')
-    echo 1\2
-    >>> print f('echo `foo`')
-    echo \`foo\`
-
-    The following code will test each command of the previous tests in
-    a real use-case, with the 'echo' built-in/program.
-
-    >>> from popen2 import popen3
-    >>> docstring = escapeShellCommand.__doc__
-    >>> lines = docstring.splitlines()
-    >>> tests_ok = True
-    >>> for i in range(3, len(lines), 2):
-    ...     line = lines[i]
-    ...     if not line: break
-    ...     command = line[line.find("'") + 1:line.rfind("'")]
-    ...     stdout, _, _ = popen3('echo %s' % escapeShellCommand(command))
-    ...     tests_ok &= stdout.read().strip() == command
-    >>> tests_ok
-    True
-    """
+    """Escape special shell characters from ``command``."""
     ## This is probaly slower than using a regexp, but definitely more
     ## readable.
     escaped = ''
