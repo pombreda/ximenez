4e3e1508ddd5c25a2ec39508dae9996788ed32ce
damien.baty <damien.baty@809b8a20-6c76-11dc-b6fd-a992d7edf03f>
2007-11-25 16:46:12 +0000

    Simplified code by sub-classing from a suitable plugin.

--- src/ximenez/collectors/zope/readlines.py
+++ src/ximenez/collectors/zope/readlines.py
@@ -22,8 +22,8 @@ that are listed in a file.
 $Id$
 """
 
-from ximenez.collectors.collector import Collector
 from ximenez.shared.zope import ZopeInstance
+from ximenez.collectors.misc.readlines import ReadLines as BaseReadlinesCollector
 
 
 def getInstance():
@@ -31,9 +31,7 @@ def getInstance():
     return ZopeInstancesReadlines()
 
 
-## FIXME: This class should sub-class 'collectors.misc.Readlines'.
-## This would simplify the implementation.
-class ZopeInstancesReadlines(Collector):
+class ZopeInstancesReadlines(BaseReadlinesCollector):
     """A collector which returns instances of Zope servers that are
     listed in a file.
 
@@ -45,32 +43,10 @@ class ZopeInstancesReadlines(Collector):
     Returns a tuple of ``ZopeInstance`` instances.
     """
 
-    _input_info = ({'name': 'path',
-                    'prompt': 'File: ',
-                    'required': True},
-                   )
-    _multiple_input = False
-
-
-    def getInput(self, cl_input=None):
-        """Get input from the user if what was provided in the command
-        line (available in ``cl_input``) was not sufficient.
-
-        If a value is given in the command line (``cl_input``), we
-        suppose it is the path of the file.
-        """
-        if cl_input:
-            self._input['path'] = cl_input
-        else:
-            ## Back to the default implementation
-            Collector.getInput(self, cl_input)
-
-
     def collect(self):
         """Return a tuple of ``ZopeInstance`` objects."""
-        lines = open(self._input['path']).readlines()
         instances = []
-        for line in lines:
+        for line in BaseReadlinesCollector.collect(self):
             host, port = line.split(':')
             port = port.strip()
             instances.append(ZopeInstance(host, port))
