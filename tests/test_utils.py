30f614fa8b54ce6939775ebb3dadfb9d6b853be0
damien.baty <damien.baty@809b8a20-6c76-11dc-b6fd-a992d7edf03f>
2007-11-26 18:36:21 +0000

    Fixed test: 'actions.misc.print' has been renamed to 'log'.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,20 +17,20 @@ class UtilsTestCase(XimenezTestCase):
 
         ## Get module by dotted name
         self.failUnless(m('misc.readlines', 'collectors'))
-        self.failUnless(m('misc.print', 'actions'))
+        self.failUnless(m('misc.log', 'actions'))
         self.failUnlessRaises(ImportError,
-                              m, 'misc.print', 'collectors')
+                              m, 'misc.log', 'collectors')
         self.failUnlessRaises(ImportError,
-                              m, 'misc.print', 'bogus_kind')
+                              m, 'misc.log', 'bogus_kind')
         self.failUnlessRaises(ImportError, m, 'misc.readlines')
 
         ## Get module by path
         base_dir = os.path.dirname(utils.__file__)
         path = os.path.join(base_dir, 'collectors', 'misc', 'readlines.py')
         self.failUnless(m(path))
-        path = os.path.join(base_dir, 'actions', 'misc', 'print.py')
+        path = os.path.join(base_dir, 'actions', 'misc', 'log.py')
         self.failUnless(m(path))
-        path = os.path.join(base_dir, 'collectors', 'misc', 'print.py')
+        path = os.path.join(base_dir, 'collectors', 'misc', 'log.py')
         self.failUnlessRaises(ImportError, m, path)
 
 
